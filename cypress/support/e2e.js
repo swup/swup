0c453f6045da4647a7b61f0803c28346f7bcf4ee
Philipp Daun <post@philippdaun.net>
2022-06-06 12:08:50 +0200

    Update E2E testing libraries

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,8 +15,4 @@ import '@cypress/code-coverage/support'
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
 import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
