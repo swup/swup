d42c776cb1c629d5969d693fa4dab4956c098ca5
Philipp Daun <post@philippdaun.net>
2022-07-26 21:59:58 +0200

    Warn and resolve immediately if no animated elements found

--- src/modules/getAnimationPromises.js
+++ src/modules/getAnimationPromises.js
@@ -4,6 +4,11 @@ import { transitionEnd } from '../helpers';
 const getAnimationPromises = function() {
 	const promises = [];
 	const animatedElements = queryAll(this.options.animationSelector, document.body);
+
+	if (!animatedElements.length) {
+		console.error(`No animated elements found by selector ${this.options.animationSelector}`);
+		return [Promise.resolve()];
+	}
 	animatedElements.forEach((element) => {
 		const promise = new Promise((resolve) => {
 			element.addEventListener(transitionEnd(), (event) => {
