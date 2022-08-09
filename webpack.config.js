1dcb5d0aa080261c0f131f383127f86f08e1eab0
Philipp Daun <post@philippdaun.net>
2022-08-09 18:49:28 +0200

    Upgrade babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,56 @@
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const baseConfig = require('@swup/webpack-config');
+const TerserPlugin = require('terser-webpack-plugin');
+// const baseConfig = require('@swup/webpack-config');
 
-const swupConfig = Object.assign({}, baseConfig, {
+const moduleConfig = {
+	mode: 'production',
+	entry: {
+		swup: './src/index.js',
+	},
+	output: {
+		path: __dirname + '/dist/',
+		filename: '[name].mjs',
+		library: {
+			type: 'module'
+		}
+	},
+	experiments: {
+		outputModule: true
+	}
+};
+
+const bundleConfig = {
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				test: /\.min\.js$/
+			})
+		]
+	},
 	entry: {
 		swup: './entry.js',
 		'swup.min': './entry.js'
 	},
 	output: {
 		path: __dirname + '/dist/',
-		library: 'Swup',
-		libraryTarget: 'umd',
-		filename: '[name].js'
+		filename: '[name].js',
+		library: {
+			type: 'umd',
+			name: 'Swup'
+		}
 	}
-});
+};
 
-module.exports = swupConfig;
+module.exports = [moduleConfig, bundleConfig];
