b9ad14d50e069594a2a2a5cbf0df61cc6058aa04
Philipp Daun <post@philippdaun.net>
2022-08-31 13:26:19 +0200

    Create history helpers

--- src/helpers/createHistoryRecord.js
+++ src/helpers/createHistoryRecord.js
@@ -1,13 +1,12 @@
-const createHistoryRecord = (url) => {
-	window.history.pushState(
-		{
-			url: url || window.location.href.split(window.location.hostname)[1],
-			random: Math.random(),
-			source: 'swup'
-		},
-		document.title,
-		url || window.location.href.split(window.location.hostname)[1]
-	);
+const createHistoryRecord = (url, customData = {}) => {
+	url = url || window.location.href.split(window.location.hostname)[1];
+	const data = {
+		url,
+		random: Math.random(),
+		source: 'swup',
+		...customData
+	};
+	window.history.pushState(data, document.title, url);
 };
 
 export default createHistoryRecord;
