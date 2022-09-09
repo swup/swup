ec93c70d908a49a647f5a2f515df013bb2bb206f
Rasso Hilber <mail@rassohilber.com>
2022-09-09 14:16:40 +0200

    Revert default cache option to `boolean` true

--- src/index.js
+++ src/index.js
@@ -39,9 +39,6 @@ export default class Swup {
 			plugins: [],
 			skipPopStateHandling: function(event) {
 				return !(event.state && event.state.source === 'swup');
-			},
-			cache: {
-				resolvePath: path => path
 			}
 		};
 
