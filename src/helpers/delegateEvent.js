584520752c970d7ca6a24c633c153e06859b16d1
Philipp Daun <post@philippdaun.net>
2022-12-14 17:25:24 +0100

    Fix empty options object

--- src/helpers/delegateEvent.js
+++ src/helpers/delegateEvent.js
@@ -1,6 +1,6 @@
 import delegate from 'delegate-it';
 
-const delegateEvent = (selector, type, callback, { base = document, ...eventOptions }) => {
+const delegateEvent = (selector, type, callback, { base = document, ...eventOptions } = {}) => {
 	const delegation = delegate(base, selector, type, callback, eventOptions);
 	return { destroy: () => delegation.destroy() };
 };
