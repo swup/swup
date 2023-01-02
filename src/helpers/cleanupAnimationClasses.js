303581baa9c99ad994d2e99cc165b40da3046e64
Philipp Daun <post@philippdaun.net>
2023-01-02 18:08:47 +0100

    Simplify class cleanup

--- src/helpers/cleanupAnimationClasses.js
+++ src/helpers/cleanupAnimationClasses.js
@@ -1,16 +1,18 @@
+const swupClasses = [
+	'is-changing',
+	'is-rendering',
+	'is-popstate',
+	/^to-/,
+];
+
 const cleanupAnimationClasses = () => {
-	document.documentElement.className.split(' ').forEach((classItem) => {
-		if (
-			// remove "to-{page}" classes
-			new RegExp('^to-').test(classItem) ||
-			// remove all other classes
-			classItem === 'is-changing' ||
-			classItem === 'is-rendering' ||
-			classItem === 'is-popstate'
-		) {
-			document.documentElement.classList.remove(classItem);
-		}
-	});
+  const htmlClasses = [...document.documentElement.classList];
+	const classesToRemove = htmlClasses.filter(
+		(htmlClass) => swupClasses.some(
+			(swupClass) => htmlClass.match(swupClass)
+		)
+	);
+	document.documentElement.classList.remove(...classesToRemove);
 };
 
 export default cleanupAnimationClasses;
