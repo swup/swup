c44622dfa4ef37cf98e8801de002ad04d05db92a
Philipp Daun <post@philippdaun.net>
2023-01-02 18:54:00 +0100

    Disable classname normalization for now

--- src/helpers/classify.js
+++ src/helpers/classify.js
@@ -1,8 +1,8 @@
 const classify = (text, fallback) => {
 	const output = String(text)
 		.toLowerCase()
-		.normalize('NFD') // split an accented letter in the base letter and the acent
-		.replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
+		// .normalize('NFD') // split an accented letter in the base letter and the acent
+		// .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
 		.replace(/[\s/_]+/g, '-') // replace spaces and slashes with '-'
 		.replace(/[^\w-]+/g, '') // remove all non-word chars
 		.replace(/--+/g, '-') // replace repeating '-' with single '-'
