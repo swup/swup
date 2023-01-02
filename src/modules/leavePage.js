4886de590bc3e5e26a9bf5f93fab52be6020887d
Philipp Daun <post@philippdaun.net>
2023-01-02 18:08:38 +0100

    Simplify class handling in leavePage

--- src/modules/leavePage.js
+++ src/modules/leavePage.js
@@ -9,19 +9,20 @@ const leavePage = function(data, { popstate = false, skipTransition = false } = 
 	this.triggerEvent('animationOutStart');
 
 	// handle classes
-	document.documentElement.classList.add('is-changing');
-	document.documentElement.classList.add('is-leaving');
-	document.documentElement.classList.add('is-animating');
-	if (popstate) {
-		document.documentElement.classList.add('is-popstate');
-	}
-	document.documentElement.classList.add(`to-${classify(data.url)}`);
+	document.documentElement.classList.toggle('is-popstate', popstate);
+	document.documentElement.classList.add(
+		'is-changing',
+		'is-leaving',
+		'is-animating',
+		`to-${classify(data.url)}`
+	);
 
 	// animation promise stuff
 	const animationPromises = this.getAnimationPromises('out');
 	Promise.all(animationPromises).then(() => {
 		this.triggerEvent('animationOutDone');
 	});
+
 	return animationPromises;
 };
 
