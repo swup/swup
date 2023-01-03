e02082fd5210dc9382e8cd58f6050e1578832e45
Philipp Daun <post@philippdaun.net>
2023-01-03 16:29:15 +0100

    Replace dot with dash instead of removing it

--- src/helpers/classify.js
+++ src/helpers/classify.js
@@ -3,7 +3,7 @@ const classify = (text, fallback) => {
 		.toLowerCase()
 		// .normalize('NFD') // split an accented letter in the base letter and the acent
 		// .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
-		.replace(/[\s/_]+/g, '-') // replace spaces and slashes with '-'
+		.replace(/[\s/_.]+/g, '-') // replace spaces and _./ with '-'
 		.replace(/[^\w-]+/g, '') // remove all non-word chars
 		.replace(/--+/g, '-') // replace repeating '-' with single '-'
 		.replace(/^-+|-+$/, ''); // trim '-' from edges
