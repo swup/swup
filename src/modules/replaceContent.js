e1f9c48f7dbf79747c670802d9750e2571c2b183
Philipp Daun <post@philippdaun.net>
2023-01-03 14:13:42 +0100

    Add comments for new replaceContent method

--- src/modules/replaceContent.js
+++ src/modules/replaceContent.js
@@ -1,13 +1,25 @@
 import { query } from '../utils.js';
 
+/**
+ * Perform the replacement of content after loading a page.
+ *
+ * This method can be replaced or augmented by plugins to allow pausing.
+ *
+ * It takes an object with the page data as return from `getPageData` and has to
+ * return a Promise that resolves once all content has been replaced and the
+ * site is ready to start animating in the new page.
+ *
+ * @param {object} page The page object
+ * @returns Promise
+ */
 const replaceContent = function({ blocks, title }) {
-	// replace blocks
+	// Replace content blocks
 	blocks.forEach((html, i) => {
 		const block = query(`[data-swup="${i}"]`, document.body);
 		block.outerHTML = html;
 	});
 
-		// set title
+	// Update browser title
 	document.title = title;
 
 	// Return a Promise to allow plugins to defer
