24c62f7de041f05443cb86f7e18425efe36043ad
Philipp Daun <post@philippdaun.net>
2023-01-03 14:45:51 +0100

    Create wrapper around fetch API

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,34 +1,22 @@
-const fetch = (setOptions, callback = false) => {
-	let defaults = {
-		url: window.location.pathname + window.location.search,
-		method: 'GET',
-		data: null,
-		headers: {}
-	};
-
-	let options = {
-		...defaults,
-		...setOptions
-	};
-
-	let request = new XMLHttpRequest();
-
-	request.onreadystatechange = function() {
-		if (request.readyState === 4) {
-			if (request.status !== 500) {
-				callback(request);
-			} else {
-				callback(request);
-			}
-		}
-	};
-
-	request.open(options.method, options.url, true);
-	Object.keys(options.headers).forEach((key) => {
-		request.setRequestHeader(key, options.headers[key]);
-	});
-	request.send(options.data);
-	return request;
+const defaults = {
+	url: window.location.pathname + window.location.search,
+	method: 'GET',
+	headers: {}
 };
 
-export default fetch;
+const fetchWrapper = (options, callback = false) => {
+	const { url, method, data, body, headers } = { ...defaults, ...options };
+
+	const init = { method, headers, body: body || data };
+
+	return fetch(url, init)
+		.then((response) => response.text())
+		.then((html) => {
+			// Compatibility layer for other methods expecting XHR properties
+			response.responseURL = response.url;
+			response.responseText = html;
+			callback(response);
+		});
+};
+
+export default fetchWrapper;
