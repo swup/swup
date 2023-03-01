d41702ba4589105259d4bda322f049544b6d1759
Philipp Daun <post@philippdaun.net>
2023-03-01 17:58:10 +0100

    Add test to check for request headers

--- cypress/e2e/main.spec.js
+++ cypress/e2e/main.spec.js
@@ -11,10 +11,21 @@ describe('Request', function () {
 	});
 
 	it('should send the correct referer', function () {
+		const referer = `${baseUrl}/page-1.html`;
 		cy.intercept('GET', '/page-2.html').as('request');
 		cy.triggerClickOnLink('/page-2.html');
-		const referer = `${baseUrl}/page-1.html`;
 		cy.wait('@request').its('request.headers.referer').should('eq', referer);
+	});
+
+	it('should send the correct request headers', function () {
+		const expected = this.swup.options.requestHeaders;
+		cy.intercept('GET', '/page-3.html').as('request');
+		cy.triggerClickOnLink('/page-3.html');
+		cy.wait('@request').its('request.headers').then((headers) => {
+			Object.entries(expected).forEach(([header, value]) => {
+				cy.wrap(headers).its(header.toLowerCase()).should('eq', value);
+			});
+		});
 	});
 });
 
