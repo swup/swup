850fd7d7c89d936484279806fe390b1369f098c9
Rasso Hilber <mail@rassohilber.com>
2023-06-26 12:38:24 +0200

    Normalize the URL in `getCurrentUrl`

--- src/helpers/getCurrentUrl.ts
+++ src/helpers/getCurrentUrl.ts
@@ -1,3 +1,6 @@
+import { Location } from './Location.js';
+
 export const getCurrentUrl = ({ hash }: { hash?: boolean } = {}): string => {
-	return location.pathname + location.search + (hash ? location.hash : '');
+	const { pathname, search } = window.location;
+	return Location.fromUrl(pathname + search).url + (hash ? window.location.hash : '');
 };
