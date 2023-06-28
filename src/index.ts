0afbe4555c7ba2a80e6c2c818f0a5dd5743b3ea5
Philipp Daun <post@philippdaun.net>
2023-06-28 17:28:38 +0200

    Explicitly import and export types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import Swup, { Options } from './Swup.js';
-import { CacheData } from './modules/Cache.js';
-import { Context, PageContext } from './modules/Context.js';
-import { Plugin } from './modules/plugins.js';
-import { HookDefinitions, Handler } from './modules/Hooks.js';
+import Swup, { type Options } from './Swup.js';
+import type { CacheData } from './modules/Cache.js';
+import type { Context, PageContext } from './modules/Context.js';
+import type { Plugin } from './modules/plugins.js';
+import type { HookDefinitions, Handler } from './modules/Hooks.js';
 
 export default Swup;
 
 export * from './helpers.js';
 export * from './utils.js';
 
-export { Options, Plugin, CacheData, Context, PageContext, HookDefinitions, Handler };
+export type { Options, Plugin, CacheData, Context, PageContext, HookDefinitions, Handler };
