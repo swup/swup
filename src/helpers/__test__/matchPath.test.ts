51c0b8c62faf8afd35b21009521b8587b7b444e4
Rasso Hilber <mail@rassohilber.com>
2023-06-29 12:27:09 +0200

    Does this have to do with the named export?

--- src/helpers/__test__/matchPath.test.ts
+++ src/helpers/__test__/matchPath.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { matchPath } from '../../Swup.js';
+import { matchPath } from '../matchPath.js';
 import { pathToRegexp } from 'path-to-regexp';
 
 /**
