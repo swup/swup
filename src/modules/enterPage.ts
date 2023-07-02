f84fea54174ebdee4628a74619a37428164d0838
Philipp Daun <post@philippdaun.net>
2023-07-02 22:27:53 +0200

    Revert "Add back nextTick calls in enterPage"
    This reverts commit 5924e09dc2c71f10b4f287e1742a90aeef9a9349.

--- src/modules/enterPage.ts
+++ src/modules/enterPage.ts
@@ -1,19 +1,18 @@
 import Swup from '../Swup.js';
-import { nextTick } from '../utils.js';
 
 export const enterPage = async function (this: Swup) {
 	if (this.context.transition.animate) {
-		await nextTick();
-		await this.hooks.trigger('animationInStart', undefined, () => {
-			document.documentElement.classList.remove('is-animating');
-		});
-		await this.hooks.trigger(
+		const animation = this.hooks.trigger(
 			'awaitAnimation',
 			{ selector: this.options.animationSelector, direction: 'in' },
 			async (context, { selector, direction }) => {
 				await Promise.all(this.getAnimationPromises({ selector, direction }));
 			}
 		);
+		await this.hooks.trigger('animationInStart', undefined, () => {
+			document.documentElement.classList.remove('is-animating');
+		});
+		await animation;
 		await this.hooks.trigger('animationInDone');
 	}
 
