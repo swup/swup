b8cd2175776fccb352a201eee8b8f49f2663c657
Philipp Daun <post@philippdaun.net>
2023-07-03 21:21:50 +0200

    Get basic tests working

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,9 +1,30 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
+
+import Swup from '../src/Swup.js';
+
+declare global {
+  interface Window {
+    _swup: Swup;
+  }
+}
+
+function swup(page: Page) {
+  return page.evaluate(() => window._swup);
+}
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/page-1.html', { waitUntil: 'load' });
+})
 
 test.describe('request', () => {
-  test('does things', async ({ page }) => {
-    await page.goto('/page-1.html');
+  test('arrives', async ({ page }) => {
     await expect(page).toHaveURL('/page-1.html');
     await expect(page).toHaveTitle('Page 1');
+  });
+
+  test('visits', async ({ page }) => {
+    await page.click('a[href="/page-2.html"]');
+    await expect(page).toHaveURL('/page-2.html');
+    await expect(page).toHaveTitle('Page 2');
   });
 });
