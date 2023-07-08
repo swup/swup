e4e4929c5a9593e8fbcf56f1c30c9c2ee5c36215
Philipp Daun <post@philippdaun.net>
2023-07-08 14:41:22 +0200

    Remove unused import

--- src/modules/loadPage.ts
+++ src/modules/loadPage.ts
@@ -1,6 +1,6 @@
 import Swup from '../Swup.js';
 import { createHistoryRecord, updateHistoryRecord, getCurrentUrl, Location } from '../helpers.js';
-import { FetchOptions, PageData } from '../modules/fetchPage.js';
+import { FetchOptions } from '../modules/fetchPage.js';
 import { ContextInitOptions, PageContext } from './Context.js';
 
 export type HistoryAction = 'push' | 'replace';
