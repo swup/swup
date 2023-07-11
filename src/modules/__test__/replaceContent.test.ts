fd748486b18d85490b4940dd4edb5ad7a3b773c4
Rasso Hilber <mail@rassohilber.com>
2023-07-11 08:50:49 +0200

    Add tests for `replaceContent`

--- src/modules/__test__/replaceContent.test.ts
+++ src/modules/__test__/replaceContent.test.ts
@@ -4,47 +4,69 @@ import type { PageData } from '../fetchPage.js';
 
 import { JSDOM } from 'jsdom';
 
+const getHtml = (body: string): string => {
+	return /*html*/ `
+		<!DOCTYPE html>
+		<body>
+			${body}
+		</body>
+	`;
+};
+
 const getPage = (body: string): PageData => {
 	return {
 		url: '',
-		html: /*html*/ `
-				<!DOCTYPE html>
-				<body>
-					${body}
-				</body>
-			`
+		html: getHtml(body)
 	};
 };
 
-describe('replaceContent', () => {
-	beforeEach(() => {
-		const dom = new JSDOM(/*html*/ `
-			<!DOCTYPE html>
-			<body>
-				<div id="container-1"></div>
-				<div id="container-2"></div>
-			</body>
-		`);
-		vi.stubGlobal('document', dom.window.document);
-	});
+const stubDocument = (body: string = ''): void => {
+	if (!body) {
+		body = /* html */ `
+			<div id="container-1" data-from="current"></div>
+			<div id="container-2" data-from="current"></div>
+			<div id="container-3" data-from="current"></div>
+		`;
+	}
 
-	it('should replace containers', () => {
+	const dom = new JSDOM(getHtml(body));
+	vi.stubGlobal('document', dom.window.document);
+};
+
+describe('replaceContent', () => {
+	beforeEach(() => stubDocument());
+
+	it.only('should replace containers', () => {
 		const page = getPage(/*html*/ `
-			<div id="container-1" data-source="incoming"></div>
-			<div id="container-2" data-source="incoming"></div>`);
+			<div id="container-1" data-from="incoming"></div>
+			<div id="container-2" data-from="incoming"></div>`);
 		const swup = new Swup();
 
-		const result = swup.replaceContent(page, { containers: ['#container-1'] });
+		const result = swup.replaceContent(page, { containers: ['#container-1', '#container-2'] });
 
 		expect(result).toBe(true);
-		expect(document.querySelector('#container-1')?.getAttribute('data-source')).toBe(
-			'incoming'
-		);
+		expect(document.querySelector('#container-1')?.getAttribute('data-from')).toBe('incoming');
+		expect(document.querySelector('#container-2')?.getAttribute('data-from')).toBe('incoming');
+		expect(document.querySelector('#container-3')?.getAttribute('data-from')).toBe('current');
 	});
 
-	it('should return false if containers mismatch', () => {
+	it('should return false if containers are missing in current DOM', () => {
+		stubDocument(/*html*/ `
+			<div id="container-2"></div>
+		`);
 		const page = getPage(/*html*/ `
-			<div id="container-1" data-source="incoming"></div>`);
+			<div id="container-1" data-from="incoming"></div>
+			`);
+
+		const swup = new Swup();
+		const result = swup.replaceContent(page, { containers: ['#container-1', '#container-2'] });
+
+		expect(result).toBe(false);
+	});
+
+	it('should return false if containers are missing in incoming DOM', () => {
+		const page = getPage(/*html*/ `
+			<div id="container-1" data-from="incoming"></div>`);
 
 		const swup = new Swup();
 		const result = swup.replaceContent(page, { containers: ['#container-1', '#container-2'] });
