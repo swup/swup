93d6f6888a33a94227a94f17ad343b50a1891e54
Philipp Daun <post@philippdaun.net>
2023-07-26 10:22:41 +0200

    Remove unused import

--- src/modules/__test__/replaceContent.test.ts
+++ src/modules/__test__/replaceContent.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi, beforeEach, afterEach } from 'vitest';
+import { describe, expect, it, vi, afterEach } from 'vitest';
 import Swup from '../../Swup.js';
 import type { PageData } from '../fetchPage.js';
 import { JSDOM } from 'jsdom';
