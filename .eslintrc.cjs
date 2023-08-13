fec2c6fb94f3501ece7323597212516927a47229
Philipp Daun <post@philippdaun.net>
2023-08-13 17:00:33 +0200

    Add note about ESLint ignore pattern

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@
 module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
 	plugins: ['@typescript-eslint'],
+	/* First exclude everything, then re-include /src, then exclude tests */
 	ignorePatterns: ['/*', '!/src', '*.test.ts'],
 	rules: {
 		'no-unused-vars': ['error', { vars: 'all', args: 'none' }],
