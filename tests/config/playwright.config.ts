09bf0d7f1d0a33f676221e2f517dff619d57e62f
Philipp Daun <post@philippdaun.net>
2023-08-28 23:37:32 +0200

    Take screenshots on test failure

--- tests/config/playwright.config.ts
+++ tests/config/playwright.config.ts
@@ -51,6 +51,8 @@ export default defineConfig({
     baseURL: baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    /* Capture screenshot after each test failure. */
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
