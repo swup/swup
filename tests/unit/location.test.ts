b0e0fc0709851eaf7d094bdc166349590bb29b30
Philipp Daun <post@philippdaun.net>
2023-11-15 21:36:50 +0100

    Expand Location helper tests

--- tests/unit/location.test.ts
+++ tests/unit/location.test.ts
@@ -6,12 +6,21 @@ describe('Location', () => {
 		const location = new Location('/path?query#anchor');
 		expect(location.href).to.eq('http://localhost:3000/path?query#anchor');
 	});
-	it('accepts a base', () => {
-		const location = new Location('/path?query#anchor', 'http://example.net');
-		expect(location.href).to.eq('http://example.net/path?query#anchor');
-	});
 	it('has a url', () => {
 		const location = new Location('/path?query#anchor');
+		expect(location.url).to.eq('/path?query');
+	});
+	it('accepts a base', () => {
+		const location = new Location('/path?query#anchor', 'http://base.net');
+		expect(location.href).to.eq('http://base.net/path?query#anchor');
+	});
+	it('accepts absolute URLs', () => {
+		const location = new Location('http://example.net/path?query#anchor', 'http://base.net');
+		expect(location.href).to.eq('http://example.net/path?query#anchor');
+	});
+	it('allows static creation', () => {
+		const location = Location.fromUrl('http://example.net/path?query#anchor');
+		expect(location.href).to.eq('http://example.net/path?query#anchor');
 		expect(location.url).to.eq('/path?query');
 	});
 });
