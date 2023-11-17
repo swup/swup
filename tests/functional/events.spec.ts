3d843fd9b3583ebc2e911faa175badcb4420cf02
Rasso Hilber <mail@rassohilber.com>
2023-11-17 09:37:52 +0100

    Add test: custom dom events bubble to window

--- tests/functional/events.spec.ts
+++ tests/functional/events.spec.ts
@@ -7,9 +7,17 @@ test.describe('events', () => {
 		await page.goto('/page-1.html');
 	});
 
-	test('triggers custom dom events', async ({ page }) => {
+	test('triggers custom dom events on document', async ({ page }) => {
 		await page.evaluate(() => {
 			document.addEventListener('swup:link:click', (event: any) => window.data = event.detail.hook);
+		});
+		await clickOnLink(page, '/page-2.html');
+		expect(await page.evaluate(() => window.data)).toStrictEqual('link:click');
+	});
+
+	test('custom dom events bubble to window', async ({ page }) => {
+		await page.evaluate(() => {
+			window.addEventListener('swup:link:click', (event: any) => window.data = event.detail.hook);
 		});
 		await clickOnLink(page, '/page-2.html');
 		expect(await page.evaluate(() => window.data)).toStrictEqual('link:click');
