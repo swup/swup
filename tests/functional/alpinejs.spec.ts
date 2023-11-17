f1f55db6acca7b53366cceae2f6c3b83fe2abe81
Rasso Hilber <mail@rassohilber.com>
2023-11-17 11:23:40 +0100

    Remove unused var

--- tests/functional/alpinejs.spec.ts
+++ tests/functional/alpinejs.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
 import { waitForSwup } from '../support/swup.js';
 import { clickOnLink } from '../support/commands.js';
