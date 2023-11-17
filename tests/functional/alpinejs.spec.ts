c4942d290a6a34accd42184c24af5f31d1fb5b8d
Rasso Hilber <mail@rassohilber.com>
2023-11-17 11:17:07 +0100

    Add test for alpinejs compatibility

---
+++ tests/functional/alpinejs.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+import { waitForSwup } from '../support/swup.js';
+import { clickOnLink } from '../support/commands.js';
+import { prefixed } from '../support/utils.js'
+
+const url = prefixed('/alpinejs/');
+
+test.describe('alpinejs compatibility', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto(url('/page-1.html'));
+		await waitForSwup(page);
+	});
+
+	test('should listen to dom events', async ({ page }) => {
+		await clickOnLink(page, url('/page-2.html'));
+		await page.waitForSelector('.alpine-component.click-fired');
+		await page.waitForSelector('.alpine-component.all-fired');
+	});
+});
