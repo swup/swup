fe758c04bb6dc8bf310264075fd374fbbeac9502
Philipp Daun <post@philippdaun.net>
2023-12-01 14:53:47 +0100

    Add tests for native animation timing

--- tests/functional/animation-timing.spec.ts
+++ tests/functional/animation-timing.spec.ts
@@ -3,20 +3,29 @@ import { test } from '@playwright/test';
 import { expectSwupAnimationDuration } from '../support/swup.js';
 
 test.describe('animation timing', () => {
-	test.skip(({ browserName }) => browserName == 'webkit', 'WebKit measurements are off');
+	test.skip(({ browserName }) => browserName === 'webkit', 'WebKit measurements are off');
 
 	test('detects animation timing', async ({ page }) => {
 		await page.goto('/animation-duration.html');
-		await expectSwupAnimationDuration(page, 400);
+		await expectSwupAnimationDuration(page, { out: 400, in: 400 });
 	});
 
 	test('detects complex animation timing', async ({ page }) => {
 		await page.goto('/animation-complex.html');
-		await expectSwupAnimationDuration(page, 600);
+		await expectSwupAnimationDuration(page, { out: 600, in: 600 });
 	});
 
 	test('detects keyframe timing', async ({ page }) => {
 		await page.goto('/animation-keyframes.html');
-		await expectSwupAnimationDuration(page, 700);
+		await expectSwupAnimationDuration(page, { out: 700, in: 700 });
+	});
+});
+
+test.describe('native view transitions', () => {
+	test.skip(({ browserName }) => browserName !== 'chromium', 'View transitions only supported in Chromium');
+
+	test('awaits native view transitions', async ({ page }) => {
+		await page.goto('/animation-native.html');
+		await expectSwupAnimationDuration(page, { total: 500 });
 	});
 });
