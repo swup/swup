4632211789d077cbe04fb9c523dc3be9c43bbb48
Philipp Daun <post@philippdaun.net>
2023-12-01 15:11:36 +0100

    Expect total duration in tests

--- tests/functional/animation-timing.spec.ts
+++ tests/functional/animation-timing.spec.ts
@@ -7,17 +7,17 @@ test.describe('animation timing', () => {
 
 	test('detects animation timing', async ({ page }) => {
 		await page.goto('/animation-duration.html');
-		await expectSwupAnimationDuration(page, { out: 400, in: 400 });
+		await expectSwupAnimationDuration(page, { out: 400, in: 400, total: 800 });
 	});
 
 	test('detects complex animation timing', async ({ page }) => {
 		await page.goto('/animation-complex.html');
-		await expectSwupAnimationDuration(page, { out: 600, in: 600 });
+		await expectSwupAnimationDuration(page, { out: 600, in: 600, total: 1200 });
 	});
 
 	test('detects keyframe timing', async ({ page }) => {
 		await page.goto('/animation-keyframes.html');
-		await expectSwupAnimationDuration(page, { out: 700, in: 700 });
+		await expectSwupAnimationDuration(page, { out: 700, in: 700, total: 1400 });
 	});
 });
 
