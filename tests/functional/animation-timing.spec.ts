3916344f7e24d0836fa334a0816e8fbbfaf7fafd
Rasso Hilber <mail@rassohilber.com>
2024-01-05 20:04:18 +0100

    Add test for keeping `is-leaving` until right before replacing the content

--- tests/functional/animation-timing.spec.ts
+++ tests/functional/animation-timing.spec.ts
@@ -1,6 +1,8 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import { expectSwupAnimationDuration } from '../support/swup.js';
+import { clickOnLink, expectToBeAt, sleep } from '../support/commands.js';
+import { nextTick } from '../../src/utils.js';
 
 test.describe('animation timing', () => {
 	test.skip(({ browserName }) => browserName === 'webkit', 'WebKit measurements are off');
@@ -18,5 +20,23 @@ test.describe('animation timing', () => {
 	test('detects keyframe timing', async ({ page }) => {
 		await page.goto('/animation-keyframes.html');
 		await expectSwupAnimationDuration(page, { out: 700, in: 700, total: 1400 });
+	});
+
+	test('keeps is-leaving until right before replacing the content', async ({ page }) => {
+		await page.goto('/page-1.html');
+		// @see https://stackoverflow.com/a/61336937/586823
+		await page.exposeFunction('sleep', sleep);
+		await page.evaluate(() => {
+			window.data = {};
+			window._swup.hooks.before('content:replace', async (visit) => {
+				await sleep(1000);
+				window.data = window.document.documentElement.classList.toString();
+			});
+		});
+		await clickOnLink(page, '/page-2.html');
+		await expectToBeAt(page, '/page-2.html', 'Page 2');
+		expect(await page.evaluate(() => window.data)).toBe(
+			'swup-enabled is-changing is-animating is-leaving'
+		);
 	});
 });
