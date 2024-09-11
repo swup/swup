a4aa174de51f8e8294060a86551b5c2372b2827a
Rasso Hilber <mail@rassohilber.com>
2024-09-11 10:51:16 +0200

    Fix and de-duplicate types in `matchPath`

--- src/helpers/matchPath.ts
+++ src/helpers/matchPath.ts
@@ -1,23 +1,16 @@
 import { match } from 'path-to-regexp';
 
-import type {
-	Path,
-	ParseOptions,
-	TokensToRegexpOptions,
-	RegexpToFunctionOptions,
-	MatchFunction
-} from 'path-to-regexp';
+import type { Path, ParamData } from 'path-to-regexp';
 
 export { type Path };
 
 /** Create a match function from a path pattern that checks if a URLs matches it. */
-export const matchPath = <P extends object = object>(
-	path: Path,
-	options?: ParseOptions & TokensToRegexpOptions & RegexpToFunctionOptions
-): MatchFunction<P> => {
+export const matchPath = <P extends ParamData>(
+	...args: Parameters<typeof match>
+): ReturnType<typeof match> => {
 	try {
-		return match<P>(path, options);
+		return match<P>(...args);
 	} catch (error) {
-		throw new Error(`[swup] Error parsing path "${String(path)}":\n${String(error)}`);
+		throw new Error(`[swup] Error parsing path "${String(args[0])}":\n${String(error)}`);
 	}
 };
