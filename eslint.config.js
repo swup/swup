0cd4fca96e0b7c532c8b3f5ed3b84d1863e745cc
Philipp Daun <post@philippdaun.net>
2024-09-24 15:15:39 +0200

    Upgrade eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,12 @@
 /* eslint-env node */
-module.exports = {
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended-type-checked',
