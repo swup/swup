14b348ddc47d92eab78b66af2b93e3f58eec79f7
Philipp Daun <post@philippdaun.net>
2024-09-24 15:38:07 +0200

    Upgrade to flat eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,36 @@
 /* eslint-env node */
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default {
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended-type-checked',
-		'plugin:prettier/recommended', // needs to be last
-	],
-	plugins: ['@typescript-eslint'],
-	/* First exclude everything, then re-include /src, then exclude tests */
-	ignorePatterns: ['/*', '!/src', '*.test.ts'],
-	rules: {
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none' }],
-		'@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-		'@typescript-eslint/unbound-method': 'off',
-		'@typescript-eslint/no-floating-promises': 'off',
-		'@typescript-eslint/no-redundant-type-constituents': 'off'
+export default [
+	includeIgnoreFile(gitignorePath),
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		ignores: [
+			'/',
+			'/*.*',
+			'!src/',
+			'**/*.test.ts'
+		]
 	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: true,
-		tsconfigRootDir: __dirname
-	},
-	root: true
-};
+	{
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none' }],
+			'@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+			'@typescript-eslint/unbound-method': 'off',
+			'@typescript-eslint/no-floating-promises': 'off',
+			'@typescript-eslint/no-redundant-type-constituents': 'off'
+		},
+	}
+];
