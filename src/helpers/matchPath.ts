05739c907bf3d5e0ca3af589c3577b9519fe3b06
Rasso Hilber <mail@rassohilber.com>
2025-04-29 10:15:18 +0200

    Infer the `Parameters` of `match` for simpler updates in the future

--- src/helpers/matchPath.ts
+++ src/helpers/matchPath.ts
@@ -1,19 +1,15 @@
 import { match } from 'path-to-regexp';
 
-import type {
-	Path,
-	ParseOptions,
-	TokensToRegexpOptions,
-	RegexpToFunctionOptions,
-	MatchFunction
-} from 'path-to-regexp';
+import type { Path, MatchFunction } from 'path-to-regexp';
 
 export { type Path };
 
+type Params = Parameters<typeof match>;
+
 /** Create a match function from a path pattern that checks if a URLs matches it. */
 export const matchPath = <P extends object = object>(
-	path: Path,
-	options?: ParseOptions & TokensToRegexpOptions & RegexpToFunctionOptions
+	path: Params[0],
+	options?: Params[1]
 ): MatchFunction<P> => {
 	try {
 		return match<P>(path, options);
