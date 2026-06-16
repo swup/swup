8703a2af744c7f662cda0739b78c3552b3f70f97
Philipp Daun <post@philippdaun.net>
2026-06-16 11:04:54 +0200

    Add back ESM bundle for e2e tests

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-	entry: ['src/index.ts'],
-	format: ['esm'],
-	platform: 'browser',
-	dts: true,
-	clean: true,
-	minify: true,
-	deps: { onlyBundle: false },
-});
+export default defineConfig([
+	/* ESM: main build */
+	{
+		entry: ['src/index.ts'],
+		format: ['esm'],
+		platform: 'browser',
+		dts: true,
+		clean: true,
+		minify: true,
+		deps: { onlyBundle: false },
+	},
+	/* ESM bundle: for e2e testing */
+	{
+		entry: { 'index.bundle': 'src/index.ts' },
+		format: ['esm'],
+		platform: 'browser',
+		minify: true,
+		deps: { alwaysBundle: [/.*/], onlyBundle: false }
+	},
+]);
