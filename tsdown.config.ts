08edc3b13ab6d2a2684a70a22e1811406c3d7b79
Philipp Daun <post@philippdaun.net>
2026-06-16 10:38:52 +0200

    Remove dts file clean hack

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from 'tsdown';
-import { rename, readdir } from 'node:fs/promises';
-import { join } from 'node:path';
 
 export default defineConfig({
 	entry: ['src/index.ts'],
@@ -10,18 +8,4 @@ export default defineConfig({
 	clean: true,
 	minify: true,
 	deps: { onlyBundle: false },
-	hooks: {
-		'build:done': cleanDtsFiles
-	}
 });
-
-// Rename hashed .d.ts files to stable names (e.g., index-abc123.d.ts -> index.d.ts)
-async function cleanDtsFiles() {
-	const files = await readdir('dist');
-	for (const file of files) {
-		if (file.match(/^\w+-[^.]+(\.d\.(m|c)?ts)(\.map)?$/)) {
-			const clean = file.replace(/^(\w+)-[^.]+\./, '$1.');
-			await rename(join('dist', file), join('dist', clean));
-		}
-	}
-}
